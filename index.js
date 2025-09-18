2e6723596edc4b7b41068e0235690830a2d19bff
Leo Maskell <22maskelll@bluecoatstudent.org.uk>
2025-09-18 14:52:53 +0000

    fixed styles then

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const app = express();
 
 app.use(express.json());
 
+
+// pages
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/frontend/index.html');
 });
 
 app.get('/home.html', (req, res) => {
     res.sendFile(__dirname + '/frontend/home.html');
+});
+
+app.get('/signIn.html', (req, res) => {
+    res.sendFile(__dirname + '/frontend/signIn.html');
+});
+
+app.get('/signUp.html', (req, res) => {
+    res.sendFile(__dirname + '/frontend/signUp.html');
 });
 
 // static files
