66bb86ccbbc10e5304a8c512e08898d38ea88a81
Leo Maskell <22maskelll@bluecoatstudent.org.uk>
2025-09-18 08:46:11 +0000

    added the stylesheet

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/frontend/index.html');
+});
+
+app.get('/home.html', (req, res) => {
+    res.sendFile(__dirname + '/frontend/home.html');
+});
+
+// static files
+app.get('/style.css', (req, res) => {
+    res.sendFile(__dirname + '/static/style.css');
 });
 
 app.listen(port, () => {
