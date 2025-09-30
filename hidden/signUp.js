79bcfa421625a6ee8d20c683f657dd5cd8712206
Leo Maskell <22maskelll@bluecoatstudent.org.uk>
2025-09-30 15:29:04 +0000

    sign up sheet has two passwords that actually have to be the same

--- hidden/signUp.js
+++ hidden/signUp.js
@@ -1,22 +1,26 @@
 document.getElementById("sign-up-btn").addEventListener("click", async (e) => {
     e.preventDefault();
-    console.log('the user is attempting to do it');
     let email = document.getElementById("UpEmail").value;
     let username = document.getElementById("UpUsername").value;
     let password = document.getElementById("UpPassword").value;
     let passwordConfirm = document.getElementById("UpPasswordConfirm").value;
 
-    try {
-        let response = await fetch("/api/signUp", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ email, username, password })
-        });
+    if (password === passwordConfirm) {
+        try {
+            let response = await fetch("/api/signUp", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, username, password })
+            });
 
-        let result = await response.json();
-        console.log(result);
-        alert(result.message);
-    } catch (error) {
-        console.error("Error:", error);
+            let result = await response.json();
+            console.log(result);
+            alert(result.message);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    } else {
+        console.log('user is an idiot!');
+        alert('learn to make a password that works idiot!')
     }
 });
